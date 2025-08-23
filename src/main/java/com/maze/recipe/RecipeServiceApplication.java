bface35c04495a55a9d041649a1448e5497035eb
gmhill <grant@thecodeswitcher.dev>
2025-08-23 18:31:07 -0400

    Fix package name

--- src/main/java/com/maze/recipe/RecipeServiceApplication.java
+++ src/main/java/com/maze/recipe/RecipeServiceApplication.java
@@ -1,4 +1,4 @@
-package com.maze.recipe_service;
+package com.maze.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
