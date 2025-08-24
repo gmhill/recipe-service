4065cc5727c26ce9f555a6b2de6e1c631491e9ca
gmhill <grant@thecodeswitcher.dev>
2025-08-23 21:24:18 -0400

    Remove empty test

--- src/test/java/com/maze/recipe/RecipeApplicationTests.java
+++ src/test/java/com/maze/recipe/RecipeApplicationTests.java
@@ -1,13 +1,10 @@
 package com.maze.recipe;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RecipeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
